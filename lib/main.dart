3c1720ebe1410dad3c567e9e6eac1d5663455dad
Christian Albert <calbert1209@gmail.com>
2021-05-20 01:04:42 +0900

    clean up main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:guldfasan/app_state.dart';
 import 'package:guldfasan/widgets/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  int initValue = await Future.delayed(Duration(milliseconds: 4000), () => 0);
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => AppState(),
-        ),
-      ],
-      child: MyApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Guldfasan',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: HomePage(title: 'Guldfasan'),
+      home: ChangeNotifierProvider<AppState>(
+        create: (BuildContext context) => AppState(),
+        child: HomePage(title: 'Guldfasan'),
+      ),
     );
   }
 }
