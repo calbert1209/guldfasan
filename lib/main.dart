1b98734f4091dde97df2d2a9204add0735daf2e0
Christian Albert <calbert1209@gmail.com>
2026-03-03 14:48:06 +0900

    fix theme color

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,8 @@ class App extends StatelessWidget {
       title: 'Guldfasan',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: customAmber(),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        useMaterial3: true,
       ),
       home: ChangeNotifierProvider<AppState>(
         create: (BuildContext context) => AppState(_dbService, mainReceivePort),
